6b9c8d8c445e36035e81af03c94ea50d56eb38d7
Developer <developer@iMac.local>
2019-08-02 16:40:10 +0300

    Изменения для версии 8.6.0

--- 07_3dModels_6_3dModelAtGeoLocation/js/3dmodelatgeolocation.js
+++ 07_3dModels_6_3dModelAtGeoLocation/js/3dmodelatgeolocation.js
@@ -45,54 +45,8 @@ var World = {
     },
 
     worldLoaded: function worldLoadedFn() {
-//        document.getElementById("loadingMessage").style.display = "none";
-        World.showUserInstructions("Ready 6.");
-    },
-
-    locationChanged: function locationChangedFn(lat, lon, alt, acc) {
-
-        World.showUserInstructions("lat: " + lat + " lon: " + lon + " alt: " + alt);
-//        if (!World.initiallyLoadedData) {
-//
-//            var indicatorImage = new AR.ImageResource("assets/indi.png");
-//            World.indicatorDrawable = new AR.ImageDrawable(indicatorImage, 0.1, {
-//                                                               verticalAnchor: AR.CONST.VERTICAL_ANCHOR.TOP
-//                                                           });
-//
-//            World.targetLocation = new AR.GeoLocation(59.000573, 30.334724, AR.CONST.UNKNOWN_ALTITUDE);
-//
-//            World.loadPoisFromJsonData();
-//            World.createModelAtLocation();
-//            World.initiallyLoadedData = true;
-//        }
-//
-//        // store user's current location in World.userLocation, so you always know where user is
-//        World.userLocation = {
-//            'latitude': lat,
-//            'longitude': lon,
-//            'altitude': alt,
-//            'accuracy': acc
-//        };
-//
-//        if (World.targetLocation)
-//        {
-//            World.stateOnDistance();
-//
-//            var latDirection = World.targetLocation.latitude - World.userLocation.latitude;
-//            var lonDirection = World.targetLocation.longitude - World.userLocation.longitude;
-//        }
-    },
-
-    showUserInstructions: function showUserInstructionsFn(message) {
-        document.getElementById('loadingMessage').innerHTML = message;
-    },
-
-    formatNum: function formatNumFn(num, decimals) {
-        var sign = num >= 0 ? 1 : -1;
-        return (Math.round((num*Math.pow(10,decimals)) + (sign*0.001)) / Math.pow(10,decimals)).toFixed(decimals);
+        document.getElementById("loadingMessage").style.display = "none";
     }
 };
 
 World.init();
-
-AR.context.onLocationChanged = World.locationChanged;
